566f0e10c076eb7c6e7579adab1705a973049985
NattSass67 <nattanun.sk@gmail.com>
2024-05-29 19:27:17 +0700

    fix-error112

--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -1,18 +1,85 @@
-'use client';
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
-import '@/styles/global.css';
-
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { persistor, store } from '@/session/store';
+import LocaleSwitcher from '@/components/LocaleSwitcher';
+import { BaseTemplate } from '@/templates/BaseTemplate';
 
 export default function Layout(props: { children: React.ReactNode }) {
+  const t = useTranslations('RootLayout');
+
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div className="">{props.children}</div>
-      </PersistGate>
-    </Provider>
+    <BaseTemplate
+      leftNav={
+        <>
+          <li>
+            <Link
+              href="/"
+              className="border-none text-gray-700 hover:text-gray-900"
+            >
+              {t('home_link')}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about/"
+              className="border-none text-gray-700 hover:text-gray-900"
+            >
+              {t('about_link')}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/guestbook/"
+              className="border-none text-gray-700 hover:text-gray-900"
+            >
+              {t('guestbook_link')}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/portfolio/"
+              className="border-none text-gray-700 hover:text-gray-900"
+            >
+              {t('portfolio_link')}
+            </Link>
+          </li>
+          <li>
+            <a
+              className="border-none text-gray-700 hover:text-gray-900"
+              href="https://github.com/ixartz/Next-js-Boilerplate"
+            >
+              GitHub
+            </a>
+          </li>
+        </>
+      }
+      rightNav={
+        <>
+          <li>
+            <Link
+              href="/sign-in/"
+              className="border-none text-gray-700 hover:text-gray-900"
+            >
+              {t('sign_in_link')}
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/sign-up/"
+              className="border-none text-gray-700 hover:text-gray-900"
+            >
+              {t('sign_up_link')}
+            </Link>
+          </li>
+
+          <li>
+            <LocaleSwitcher />
+          </li>
+        </>
+      }
+    >
+      <div className="py-5 text-xl [&_p]:my-6">{props.children}</div>
+    </BaseTemplate>
   );
 }
