7a91119043b1406b9d5dfc16b8b120511d1e434f
NattSass67 <nattanun.sk@gmail.com>
2024-05-29 19:19:17 +0700

    fix-error

--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import '@/styles/global.css';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from '@/session/store';
+
 export default function Layout(props: { children: React.ReactNode }) {
-  return <div className="">{props.children}</div>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="">{props.children}</div>
+      </PersistGate>
+    </Provider>
+  );
 }
